6e0b0f6dd0c8d223c032a5963df8e5ccc90cc27a
MRV <sabudilovskiy@yandex.ru>
2022-06-01 01:44:52 +0300

    111

--- includes/SafeQueue.h
+++ includes/SafeQueue.h
@@ -1,46 +1,38 @@
-//
-// Created by MRV on 5/29/2022.
-//
-
 #ifndef TEPLINSKIH_29_05_2022_THREAD_SAFE_QUEUE_H
 #define TEPLINSKIH_29_05_2022_THREAD_SAFE_QUEUE_H
 #include <queue>
 #include <vector>
-#include <functional>
 #include "Windows.h"
-class Thread_safe_queue{
-    std::queue<std::vector<int>> values;
-    std::queue<std::function<void(std::vector<int>&)>> actions;
+template<class T>
+class SafeQueue{
+    std::queue<T> values;
     HANDLE event;
 public:
     bool empty(){
-        return values.empty() && actions.empty();
+        return values.empty();
     }
-    void push(std::vector<int> value, std::function<void(std::vector<int>&)>action ){
+    void push(T value){
         if (WaitForSingleObject(event, INFINITE) == WAIT_OBJECT_0){
             values.push(value);
-            actions.push(action);
         }
         SetEvent(event);
     }
-    bool try_pop(std::vector<int>& value, std::function<void(std::vector<int>&)>& action ){
+    bool try_pop(T& value){
         bool result = false;
         if (WaitForSingleObject(event, INFINITE) == WAIT_OBJECT_0){
             if (!empty()){
                 value = values.front();
-                action = actions.front();
                 result = true;
-                actions.pop();
                 values.pop();
             }
         }
         SetEvent(event);
         return result;
     }
-    Thread_safe_queue(){
+    SafeQueue(){
         event = CreateEvent(nullptr, false, true, nullptr);
     }
-    ~Thread_safe_queue(){
+    ~SafeQueue(){
         CloseHandle(event);
     }
 };
--- main.cpp
+++ main.cpp
@@ -1,15 +1,61 @@
 #include <iostream>
-#include "Thread_pool.h"
-int main(){
-    setlocale(LC_ALL, "rus");
-    Thread_pool tp(2, 2, 20);
-    tp.wait_all();
-    auto answer = tp.get_answers();
-    for (auto& i : answer){
-        for (auto& j : i){
-            std::cout << j << ' ';
+#include <atomic>
+#include <vector>
+#include <thread>
+#include "SafeQueue.h"
+#include "Random.h"
+
+void producer(SafeQueue<std::vector<int>>& que, std::atomic<long>& work_producer )
+{
+    while (work_producer.load() > 0){
+        work_producer.fetch_sub(1);
+        std::vector<int> vector;
+        vector.resize(20);
+        for (auto& i : vector){
+            i = my_random_int(-10, 10);
         }
-        std::cout << "Длина: " <<  i.size() <<  '\n';
+        que.push(vector);
     }
+}
+void consumer(SafeQueue<std::vector<int>>& que, std::atomic<long>& work_comsumer, SafeQueue<std::vector<int>>& answer){
+    std::vector<std::vector<int>> local_answer;
+    while (work_comsumer.load() > 0){
+        std::vector<int> temp;
+        if (que.try_pop(temp)){
+            work_comsumer.fetch_sub(1);
+            std::sort(temp.begin(),  temp.end());
+            answer.push(temp);
+        }
+    }
+}
+void task(size_t count_producers, size_t count_consumers, size_t count_vectors){
+    std::vector<std::thread> producers, consumers;
+    SafeQueue<std::vector<int>> que, answer;
+    std::atomic<long> work1 = count_vectors, work2 = count_vectors;
+    producers.resize(count_producers);
+    consumers.resize(count_consumers);
+    for (int i = 0; i < count_producers; i++){
+        producers[i] = std::thread(producer, std::ref(que), std::ref(work1));
+    }
+    for (int i = 0; i < count_consumers; i++){
+        consumers[i] = std::thread(consumer, std::ref(que), std::ref(work2), std::ref(answer));
+    }
+    for (int i = 0; i < count_producers; i++){
+        producers[i].join();
+    }
+    for (int i = 0; i < count_consumers; i++){
+        consumers[i].join();
+    }
+    while (!answer.empty()){
+        std::vector<int> temp;
+        answer.try_pop(temp);
+        for (auto i : temp){
+            std::cout << i << ' ';
+        }
+        std::cout << '\n';
+    }
+}
+int main(){
+    task(4, 4, 10);
     std::cin.get();
 }
